45f2c62f561a0db9c107c3e4975a37ada5b9a077
Thet <thet@home.local>
2023-12-03 11:01:40 +0100

    [clean] Cleanup project structure
    * create modules from day binaries
    * sprinkle some aoc related print statements

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
-fn main() {
+use aoc::*;
 
-    
+fn main() {
+    println!("{}", ASCII_ART_BANNER);
+
+    println!("# Day 01:\n");
+    day01::run();
+    println!("\n\n# Day 02:\n");
+    day02::run();
+    println!("\n\n# Day 03:\n");
+    day03::run();
 }
